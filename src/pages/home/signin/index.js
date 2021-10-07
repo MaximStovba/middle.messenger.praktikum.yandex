5f1cc87dd731ce1a4729e3dfdd40f6d83b419720
Maxim Stovba <maxim.stovba@gmail.com>
2021-10-07 22:10:14 +0300

    signin signup

---
+++ src/pages/home/signin/index.js
@@ -0,0 +1,19 @@
+import './signin.scss';
+import '../../../components/button/button.scss';
+import '../../../components/input/input.scss';
+
+
+import { signinTempl } from './signin.tmpl';
+import { Templator } from '../../../utils/templator';
+
+const context = {
+  title: 'signin',
+};
+
+const tmpl = new Templator(signinTempl);
+const renderedTemplate = tmpl.compile(context);
+const root = document.querySelector('.root');
+
+root.innerHTML = `
+  ${renderedTemplate}
+`;
---
+++ src/pages/home/signup/index.js
@@ -0,0 +1,18 @@
+import '../../../components/button/button.scss';
+import '../../../components/input/input.scss';
+import './signup.scss';
+
+import { signupTempl } from './signup.tmpl';
+import { Templator } from '../../../utils/templator';
+
+const context = {
+  title: 'signin',
+};
+
+const tmpl = new Templator(signupTempl);
+const renderedTemplate = tmpl.compile(context);
+const root = document.querySelector('.root');
+
+root.innerHTML = `
+  ${renderedTemplate}
+`;
